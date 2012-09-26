5abce5f194ffd523609208319b2437c25e04f053
abilng <abilngeorge@gmail.com>
2012-09-26 12:39:04 +0530

    Added setup

--- src/bbone/config.py
+++
@@ -1,7 +0,0 @@
-
-username="100"
-password="welcome"
-server="192.168.1.204"
-log = '/tmp/reciever.log'
-LEVEL=4
-debug=False
--- src/gui/setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2.7
-#-*-python-2.7-
-# -*- coding: utf-8 -*-
-
-import config
-
-if __name__ == "__main__":
-    config.createConfig()
